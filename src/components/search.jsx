ed1b4e8c56211dd63552a9e1789e71cd68794c9b
Parthkarir <parthcallsmaster@gmail.com>
2026-03-10 15:36:32 +0530

    fsfsdfsdf

---
+++ src/components/search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div> 
+        <form>
+
+    <input placeholder='Search Employees' className='border p-2'>
+    <button></button>
+    
+    
+    </input>
+
+
+        </form>
+
+    </div>
+  )
+}
+
+export default Search
